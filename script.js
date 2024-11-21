fabc93351d96341a504a2b18f9648df1020b7679
luanslw-dev <luanslw@gmail.com>
2024-11-21 10:09:02 -0300

    novooo

--- script.js
+++ script.js
@@ -5,7 +5,30 @@ function carregar(){
 }
 
 function enviar(){
+  // Capturando todos os checkboxes com a classe 'termos'
+  var checkboxes = document.querySelectorAll('.refogar');
+  var refogar = [];
+
+  // Verificando quais checkboxes estão marcados
+  checkboxes.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      refogar.push(checkbox.value);
+    }
+  });
+
+
+    // Capturando todos os checkboxes com a classe 'termos'
+    var checkboxes = document.querySelectorAll('.termos');
+    var selecionados = [];
+
+    // Verificando quais checkboxes estão marcados
+    checkboxes.forEach(function(checkbox) {
+      if (checkbox.checked) {
+        selecionados.push(checkbox.value);
+      }
+    });
    
+  
 
     var data = new Date()
     var hora =data.getHours()
@@ -28,8 +51,13 @@ function enviar(){
      <p>Ola <strong>${nome}</strong></p>
     <p> Sabor de <strong>${recheio}</strong> Ao  Molho <strong>${molho}</strong></p>
        <p> No Macarão <strong>${massa}</strong></p>
-          <p>Endendeço <strong>${endereco}</strong></p>
+         <p>Endendeço <strong>${endereco}</strong></p>
+       <h6>Temperos para Refogar</h6>
+       <p>${refogar}</p>
+        
           <h6>temperos para refogar</h6>
+<strong>${ selecionados }</strong>
+
     
          <p> Seu contato e <strong>${telefone}</strong></p>
           <p> Hora do pedido <strong>${hora}:${horam}</strong></p><p>Tempo de espera de 35-min a 40-min</p>  ` 
