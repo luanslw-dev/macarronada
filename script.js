4b673f6e786abf98faee8c582247fb4003734789
luanslw-dev <luanslw@gmail.com>
2024-11-21 15:14:36 -0300

    novo

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function enviar(){
 
     var data = new Date()
     var hora =data.getHours()
-    var horam =data.getMinutes()
+    var horam =data.getUTCMinutes()
     //var fomar = window.document.getElementById('fomar')
     var nome = window.document.getElementById('nome').value;
     var endereco = window.document.getElementById('endereco').value;
@@ -41,24 +41,23 @@ function enviar(){
     var molho = window.document.querySelector('input[name="molho"]:checked').value;
     var massa = window.document.querySelector('input[name="massa"]:checked').value;
    // var refogar = window.document.querySelector('input[name="refogar1"]:checked').value;
-    var finalizar = window.document.getElementById('finalizar').value;
+    
     //let num =[nome,endereco,telefone,recheio,molho,massa,refogar,finalizar]
     
     if(nome != '' && endereco!='' && telefone!='' && recheio!='' && molho!='' && massa!=''){
-     fomar.innerHTML = `<a href="index.html">voltar</a> <h3>GOLDERE</h3>   
+     fomar.innerHTML = `<a href="index.html">voltar</a>  ${data} 
+     <h3>GOLDERE</h3>  
      <h5>MACARRODAS DELIVERY</h5>
      <h6>Segue a baixao os dados do pedido</h6>
      <p>Ola <strong>${nome}</strong></p>
     <p> Sabor de <strong>${recheio}</strong> Ao  Molho <strong>${molho}</strong></p>
        <p> No Macarão <strong>${massa}</strong></p>
          <p>Endendeço <strong>${endereco}</strong></p>
-       <h6>Temperos para Refogar</h6>
+       <h6>Ingredientes para finalizar</h6>
        <strong>${refogar}</strong>
         
           <h6>temperos para refogar</h6>
 <strong>${ selecionados }</strong>
-
-    
          <p> Seu contato e <strong>${telefone}</strong></p>
           <p> Hora do pedido <strong>${hora}:${horam}</strong></p><p>Tempo de espera de 35-min a 40-min</p>  ` 
 
